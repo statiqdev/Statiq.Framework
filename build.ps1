802393226396d6a60c1e0a380c190aeed3239efc
Dave Glick <dave@daveaglick.com>
2019-08-15 15:14:49 -0400

    Switching to the DotNetCoreInstaller task

--- build.ps1
+++ build.ps1
@@ -18,7 +18,7 @@ If (!(Test-Path $ToolsDir))
 
 if (!(Test-Path $ToolsProj))
 {
-    $projectFileContents = '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp3.0</TargetFramework></PropertyGroup></Project>'
+    $projectFileContents = '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.2</TargetFramework></PropertyGroup></Project>'
     Out-File -InputObject $projectFileContents -FilePath $ToolsProj
 }
 
