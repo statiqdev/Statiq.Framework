c6aa2d42edcae5fc688ad7c31d94fe74e2096f19
Dave Glick <dave@daveaglick.com>
2019-11-07 22:07:58 -0500

    Update Spectre.Cli

--- src/core/Statiq.App/Commands/ServeCommandSettings.cs
+++ src/core/Statiq.App/Commands/ServeCommandSettings.cs
@@ -9,7 +9,7 @@ namespace Statiq.App
         [Description("The root folder to serve (the output directory by default).")]
         public string RootPath { get; set; }
 
-        [CommandOption("--port")]
+        [CommandOption("--port <PORT>")]
         [Description("Start the preview web server on the specified port (default is 5080).")]
         public int Port { get; set; } = 5080;
 
@@ -17,11 +17,11 @@ namespace Statiq.App
         [Description("Force the use of extensions in the preview web server (by default, extensionless URLs may be used).")]
         public bool ForceExt { get; set; }
 
-        [CommandOption("--virtual-dir")]
+        [CommandOption("--virtual-dir <PATH>")]
         [Description("Serve files in the preview web server under the specified virtual directory.")]
         public string VirtualDirectory { get; set; }
 
-        [CommandOption("--content-type")]
+        [CommandOption("--content-type <TYPE>")]
         [Description("Specifies additional supported content types for the preview server as extension=contenttype.")]
         public string[] ContentTypes { get; set; }
 
