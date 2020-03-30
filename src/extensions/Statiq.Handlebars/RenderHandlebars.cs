6b192fc006270dcdb23c5794cb78a4aa7253893b
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-03-31 00:03:19 +0300

    Adds Handlebars module

---
+++ src/extensions/Statiq.Handlebars/RenderHandlebars.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Threading.Tasks;
+using HandlebarsDotNet;
+using Microsoft.Extensions.Logging;
+using Statiq.Common;
+
+namespace Statiq.Handlebars
+{
+    public class RenderHandlebars : ParallelModule
+    {
+        private readonly string _sourceKey;
+        private readonly string _destinationKey;
+        private Config<object> _model;
+        private Config<IEnumerable<KeyValuePair<string, string>>> _partials;
+        private Func<IExecutionContext, IDocument, HandlebarsConfiguration, Task<HandlebarsConfiguration>> _configure;
+
+        public RenderHandlebars()
+        {
+        }
+
+        public RenderHandlebars(string sourceKey, string destinationKey = null)
+        {
+            _sourceKey = sourceKey;
+            _destinationKey = destinationKey;
+        }
+
+        public RenderHandlebars WithModel(Config<object> model)
+        {
+            _model = model;
+            return this;
+        }
+
+        public RenderHandlebars WithPartials(Config<IEnumerable<KeyValuePair<string, string>>> partials)
+        {
+            _partials = partials;
+            return this;
+        }
+
+        public RenderHandlebars Configure(
+            Func<IExecutionContext, IDocument, HandlebarsConfiguration, HandlebarsConfiguration> configure)
+        {
+            return Configure((context, document, configuration) => Task.FromResult(configure(context, document, configuration)));
+        }
+
+        public RenderHandlebars Configure(
+            Func<IExecutionContext, IDocument, HandlebarsConfiguration, Task<HandlebarsConfiguration>> configure)
+        {
+            _configure = configure;
+            return this;
+        }
+
+        protected override async Task<IEnumerable<IDocument>> ExecuteInputAsync(IDocument input, IExecutionContext context)
+        {
+            context.LogDebug(
+                   "Processing Handlebars {0} for {1}",
+                   string.IsNullOrEmpty(_sourceKey) ? string.Empty : ("in" + _sourceKey),
+                   input.ToSafeDisplayString());
+
+            string content;
+            if (string.IsNullOrEmpty(_sourceKey))
+            {
+                content = await input.GetContentStringAsync();
+            }
+            else if (input.ContainsKey(_sourceKey))
+            {
+                content = input.GetString(_sourceKey) ?? string.Empty;
+            }
+            else
+            {
+                // Don't do anything if the key doesn't exist
+                return input.Yield();
+            }
+
+            // Configure
+            HandlebarsConfiguration configuration = new HandlebarsConfiguration();
+            if (_configure != null)
+            {
+                configuration = await _configure(context, input, configuration);
+            }
+
+            IHandlebars handlebars = HandlebarsDotNet.Handlebars.Create(configuration);
+
+            // Register partials
+            if (_partials != null)
+            {
+                foreach (KeyValuePair<string, string> partial in await _partials.GetValueAsync(input, context))
+                {
+                    handlebars.RegisterTemplate(partial.Key, partial.Value);
+                }
+            }
+
+            string result = handlebars.Compile(content)(_model is null
+                ? new DocumentWrapper(input)
+                : await _model.GetValueAsync(input, context));
+
+            return string.IsNullOrEmpty(_sourceKey)
+                ? input.Clone(await context.GetContentProviderAsync(result, MediaTypes.Html)).Yield()
+                : input
+                    .Clone(new MetadataItems
+                    {
+                        { string.IsNullOrEmpty(_destinationKey) ? _sourceKey : _destinationKey, result }
+                    })
+                    .Yield();
+        }
+
+        private class DocumentWrapper : DynamicObject
+        {
+            private readonly IDocument _document;
+
+            public DocumentWrapper(IDocument document)
+            {
+                _document = document;
+            }
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                return _document.TryGetValue(binder.Name, out result);
+            }
+        }
+    }
+}
---
+++ tests/extensions/Statiq.Handlebars.Tests/RenderHandlebarsFixture.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Statiq.Common;
+using Statiq.Testing;
+
+namespace Statiq.Handlebars.Tests
+{
+    [TestFixture]
+    public class RenderHandlebarsFixture : BaseFixture
+    {
+        public class ExecuteTests : RenderHandlebarsFixture
+        {
+            [Test]
+            public async Task RendersHandlebars()
+            {
+                // Given
+                const string input = @"<div class=""entry"">
+  <h1>{{title}}</h1>
+  <div class=""body"">
+    {{body}}
+  </div>
+</div>";
+                const string output = @"<div class=""entry"">
+  <h1>My New Post</h1>
+  <div class=""body"">
+    This is my first post!
+  </div>
+</div>";
+                TestDocument document = new TestDocument(
+                    new MetadataItems
+                    {
+                        { Keys.Title, "My New Post" },
+                        { "Body", "This is my first post!" }
+                    },
+                    input);
+                RenderHandlebars handlebars = new RenderHandlebars();
+
+                // When
+                TestDocument result = await ExecuteAsync(document, handlebars).SingleAsync();
+
+                // Then
+                result.Content.ShouldBe(output, StringCompareShould.IgnoreLineEndings);
+            }
+
+            [Test]
+            public async Task AlternateModel()
+            {
+                // Given
+                const string input = @"<div class=""entry"">
+  <h1>{{title}}</h1>
+  <div class=""body"">
+    {{body}}
+  </div>
+</div>";
+                const string output = @"<div class=""entry"">
+  <h1>My New Post</h1>
+  <div class=""body"">
+    This is my first post!
+  </div>
+</div>";
+                TestDocument document = new TestDocument(input);
+                RenderHandlebars handlebars = new RenderHandlebars().WithModel(new
+                {
+                    title = "My New Post",
+                    body = "This is my first post!"
+                });
+
+                // When
+                TestDocument result = await ExecuteAsync(document, handlebars).SingleAsync();
+
+                // Then
+                result.Content.ShouldBe(output, StringCompareShould.IgnoreLineEndings);
+            }
+
+            [Test]
+            public async Task RendersHandlebarsFromMetadata()
+            {
+                // Given
+                const string input = "{{title}}";
+                const string output = "Hello World!";
+                TestDocument document = new TestDocument(new MetadataItems
+                {
+                    { "meta", input },
+                    { "title", "Hello World!" }
+                });
+                RenderHandlebars handlebars = new RenderHandlebars("meta");
+
+                // When
+                IDocument result = await ExecuteAsync(document, handlebars).SingleAsync();
+
+                // Then
+                result.GetString("meta").ShouldBe(output, StringCompareShould.IgnoreLineEndings);
+            }
+
+            [Test]
+            public async Task RendersHandlebarsFromMetadataToNewKey()
+            {
+                // Given
+                const string input = "{{title}}";
+                const string output = "Hello World!";
+                TestDocument document = new TestDocument(new MetadataItems
+                {
+                    { "meta", input },
+                    { "title", "Hello World!" }
+                });
+                RenderHandlebars handlebars = new RenderHandlebars("meta", "meta2");
+
+                // When
+                IDocument result = await ExecuteAsync(document, handlebars).SingleAsync();
+
+                // Then
+                result.GetString("meta2").ShouldBe(output, StringCompareShould.IgnoreLineEndings);
+            }
+
+            [Test]
+            public async Task DoesNothingIfMetadataKeyDoesNotExist()
+            {
+                // Given
+                TestDocument document = new TestDocument();
+                RenderHandlebars handlebars = new RenderHandlebars("meta");
+
+                // When
+                TestDocument result = await ExecuteAsync(document, handlebars).SingleAsync();
+
+                // Then
+                result.ShouldBe(document);
+            }
+
+            [Test]
+            public async Task RendersHandlebarsWithPartial()
+            {
+                // Given
+                const string input = @"{{#names}}
+  {{> user}}
+{{/names}}";
+                const string output = @"<strong>Karen</strong><strong>Jon</strong>";
+                TestDocument document = new TestDocument(
+                    new MetadataItems
+                    {
+                        {
+                            "names", new[]
+                            {
+                                new
+                                {
+                                    name = "Karen"
+                                },
+                                new
+                                {
+                                    name = "Jon"
+                                }
+                            }
+                        }
+                    },
+                    input);
+                RenderHandlebars handlebars = new RenderHandlebars()
+                    .WithPartials(Config.FromValue(new Dictionary<string, string>
+                    {
+                        ["user"] = "<strong>{{name}}</strong>"
+                    }.AsEnumerable()));
+
+                // When
+                TestDocument result = await ExecuteAsync(document, handlebars).SingleAsync();
+
+                // Then
+                result.Content.ShouldBe(output, StringCompareShould.IgnoreLineEndings);
+            }
+        }
+    }
+}
