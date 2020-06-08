68e8a4ac59a29d35a525c4db5e7dbc1a4780ab33
Dave Glick <dave@daveaglick.com>
2020-06-08 10:01:09 -0400

    Another try

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
-dotnet run --project "build\Statiq.Framework.Build" -- %*
+dotnet run --project "build/Statiq.Framework.Build/Statiq.Framework.Build.csproj" -- %*
 set exitcode=%errorlevel%
 cd %~dp0
 exit /b %exitcode%
