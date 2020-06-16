0f0ea3dc40166acef7368fc601a54423a7aa1633
Muhammad Azeez <encryptor@outlook.com>
2020-06-16 23:07:51 +0300

    fix build error

--- src/extensions/Statiq.Html/AddRtlSupport.cs
+++ src/extensions/Statiq.Html/AddRtlSupport.cs
@@ -45,7 +45,7 @@ namespace Statiq.Html
         {
             ITreeWalker walker = htmlDocument.CreateTreeWalker(htmlDocument.DocumentElement, FilterSettings.Element);
             IElement element = (IElement)walker.ToFirst();
-            
+
             while (element != null)
             {
                 if (IsBlockElement(element))
