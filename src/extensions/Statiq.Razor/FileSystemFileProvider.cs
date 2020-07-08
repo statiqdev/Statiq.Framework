d0547674dab4330615222bbaff3b51aa82230300
Dave Glick <dave@daveaglick.com>
2020-07-07 22:10:28 -0400

    Added an informational message when ignoring view start

--- src/extensions/Statiq.Razor/FileSystemFileProvider.cs
+++ src/extensions/Statiq.Razor/FileSystemFileProvider.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using Statiq.Common;
 using IFileProvider = Microsoft.Extensions.FileProviders.IFileProvider;
@@ -18,6 +20,8 @@ namespace Statiq.Razor
     {
         private readonly ConcurrentBag<ExecutionChangeToken> _executionChangeTokens =
             new ConcurrentBag<ExecutionChangeToken>();
+        private readonly ConcurrentDictionary<string, object> _reportedViewStartOverrides =
+            new ConcurrentDictionary<string, object>();
 
         public IReadOnlyFileSystem StatiqFileSystem { get; }
 
@@ -34,6 +38,20 @@ namespace Statiq.Razor
             }
 
             string inputPath = subpath.TrimStart('/');
+            if (inputPath.Contains(RenderRazor.ViewStartPlaceholder))
+            {
+                // Display an informational message that an existing view start file is being ignored due to layout override
+                int start = inputPath.IndexOf(RenderRazor.ViewStartPlaceholder) + 1;
+                int length = inputPath.IndexOf(RenderRazor.ViewStartPlaceholder, start);
+                string requestingPath = inputPath[start..length];
+                inputPath = inputPath.Remove(start - 1, length + 1).Insert(start - 1, "_ViewStart.cshtml");
+                if (StatiqFileSystem.GetInputFile(inputPath).Exists
+                    && _reportedViewStartOverrides.TryAdd(requestingPath, null))
+                {
+                    IExecutionContext.CurrentOrNull?.LogInformation($"Existing {inputPath} file for {requestingPath} being ignored in favor of explicit layout metadata");
+                }
+                return new NotFoundFileInfo(subpath);
+            }
             IFile file = StatiqFileSystem.GetInputFile(inputPath);
             return !file.Exists ? new NotFoundFileInfo(subpath) : (IFileInfo)new StatiqFileInfo(file, inputPath);
         }
