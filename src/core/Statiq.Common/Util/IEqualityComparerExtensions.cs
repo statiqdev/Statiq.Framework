b4f94fc99d21a75f958741c0ed5c88d56645221a
Dave Glick <dave@daveaglick.com>
2020-07-26 15:08:43 -0400

    Added some extensions to get converting comparers

---
+++ src/core/Statiq.Common/Util/IEqualityComparerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Statiq.Common
+{
+    public static class IEqualityComparerExtensions
+    {
+        public static ConvertingEqualityComparer<T> ToConvertingEqualityComparer<T>(this IEqualityComparer<T> comparer) =>
+            new ConvertingEqualityComparer<T>(comparer);
+    }
+}
