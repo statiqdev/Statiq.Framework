c353b51e220be7bb2b3104060d7769b1bd43ab56
Chebotov Nikolay <spiritkola@hotmail.com>
2020-08-18 23:47:07 +0300

    Adds ReadTelegram module

---
+++ src/extensions/Statiq.Telegram/ReadTelegram.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+using Statiq.Common;
+using Telegram.Bot;
+
+namespace Statiq.Telegram
+{
+    /// <summary>
+    /// Outputs metadata for information from Telegram.
+    /// </summary>
+    /// <remarks>
+    /// This modules uses the Telegram.Bot library and associated types to submit requests to Telegram. Because
+    /// of the large number of different kinds of requests, this module does not attempt to provide a fully abstract wrapper
+    /// around the Telegram.Bot library. Instead, it simplifies the housekeeping involved in setting up an
+    /// Telegram client and requires you to provide functions that fetch whatever data you need. Each request
+    /// will be sent for each input document.
+    /// </remarks>
+    /// <category>Metadata</category>
+    public class ReadTelegram : ParallelSyncModule
+    {
+        private readonly Dictionary<string, Func<IDocument, IExecutionContext, TelegramBotClient, object>> _requests
+            = new Dictionary<string, Func<IDocument, IExecutionContext, TelegramBotClient, object>>();
+
+        private readonly TelegramBotClient _telegram;
+
+        /// <summary>
+        /// Creates a connection to the Telegram API with authenticated access.
+        /// </summary>
+        /// <param name="accessToken">The API token to use.</param>
+        /// <param name="httpClient">Http client.</param>
+        public ReadTelegram(string accessToken, HttpClient httpClient = null)
+        {
+            accessToken.ThrowIfNullOrEmpty(nameof(accessToken));
+
+            _telegram = new TelegramBotClient(accessToken, httpClient);
+        }
+
+        /// <summary>
+        /// Creates a connection to the Telegram API with authenticated access and proxy.
+        /// </summary>
+        /// <param name="accessToken">The API token to use.</param>
+        /// <param name="proxy">Proxy to use Telegram API.</param>
+        public ReadTelegram(string accessToken, IWebProxy proxy)
+        {
+            accessToken.ThrowIfNullOrEmpty(nameof(accessToken));
+
+            _telegram = new TelegramBotClient(accessToken, proxy);
+        }
+
+        /// <summary>
+        /// Submits requests timeout to the Telegram client.
+        /// </summary>
+        /// <param name="timeout">Requests timeout.</param>
+        /// <returns>The current module instance.</returns>
+        public ReadTelegram WithRequestsTimeout(TimeSpan timeout)
+        {
+            _telegram.Timeout = timeout;
+            return this;
+        }
+
+        /// <summary>
+        /// Submits a request to the Telegram client. This allows you to incorporate data from the execution context in your request.
+        /// </summary>
+        /// <param name="key">The metadata key in which to store the return value of the request function.</param>
+        /// <param name="request">A function with the request to make.</param>
+        /// <returns>The current module instance.</returns>
+        public ReadTelegram WithRequest(string key, Func<IExecutionContext, TelegramBotClient, object> request)
+        {
+            key.ThrowIfNullOrEmpty(nameof(key));
+            request.ThrowIfNull(nameof(request));
+
+            _requests[key] = (doc, ctx, telegram) => request(ctx, telegram);
+            return this;
+        }
+
+        /// <summary>
+        /// Submits a request to the Telegram client. This allows you to incorporate data from the execution context and current document in your request.
+        /// </summary>
+        /// <param name="key">The metadata key in which to store the return value of the request function.</param>
+        /// <param name="request">A function with the request to make.</param>
+        /// <returns>The current module instance.</returns>
+        public ReadTelegram WithRequest(string key, Func<IDocument, IExecutionContext, TelegramBotClient, object> request)
+        {
+            key.ThrowIfNullOrEmpty(nameof(key));
+
+            _requests[key] = request.ThrowIfNull(nameof(request));
+            return this;
+        }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<IDocument> ExecuteInput(IDocument input, IExecutionContext context)
+        {
+            ConcurrentDictionary<string, object> results = new ConcurrentDictionary<string, object>();
+            System.Threading.Tasks.Parallel.ForEach(_requests, request =>
+            {
+                context.LogDebug("Submitting {0} Telegram request for {1}", request.Key, input.ToSafeDisplayString());
+                try
+                {
+                    results[request.Key] = request.Value(input, context, _telegram);
+                }
+                catch (Exception ex)
+                {
+                    context.LogWarning("Exception while submitting {0} Telegram request for {1}: {2}", request.Key, input.ToSafeDisplayString(), ex.ToString());
+                }
+            });
+            return input.Clone(results).Yield();
+        }
+    }
+}
---
+++ tests/extensions/Statiq.Telegram.Tests/ReadTelegramFixture.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Statiq.Common;
+using Statiq.Testing;
+
+namespace Statiq.Telegram.Tests
+{
+    /// <summary>
+    /// Test fixture for <see cref="ReadTelegram"/>.
+    /// </summary>
+    [TestFixture]
+    public class ReadTelegramFixture : BaseFixture
+    {
+        /// <summary>
+        /// Tests for execute method.
+        /// </summary>
+        public class ExecuteTests : ReadTelegramFixture
+        {
+            /// <summary>
+            /// Sets the metadata from the requests.
+            /// </summary>
+            [TestCase("1234567:4TT8bAc8GHUspu3ERYn-KGcvsvGB9u_n4ddy")]
+            public async Task SetsMetadata(string accessToken)
+            {
+                // Given
+                TestDocument document = new TestDocument();
+                IModule telegram = new ReadTelegram(accessToken)
+                    .WithRequest("Foo", (ctx, yt) => 1)
+                    .WithRequest("Bar", (doc, ctx, yt) => "baz");
+
+                // When
+                TestDocument result = await ExecuteAsync(document, telegram).SingleAsync();
+
+                // Then
+                result["Foo"].ShouldBe(1);
+                result["Bar"].ShouldBe("baz");
+            }
+
+            /// <summary>
+            /// Sets the metadata from the requests with requests timeout.
+            /// </summary>
+            [TestCase("1234567:4TT8bAc8GHUspu3ERYn-KGcvsvGB9u_n4ddy")]
+            public async Task SetsMetadataWithRequestsTimeout(string accessToken)
+            {
+                // Given
+                TestDocument document = new TestDocument();
+                IModule telegram = new ReadTelegram(accessToken)
+                    .WithRequestsTimeout(new TimeSpan(0, 0, 1))
+                    .WithRequest("Foo", (ctx, yt) => 1)
+                    .WithRequest("Bar", (doc, ctx, yt) => "baz");
+
+                // When
+                TestDocument result = await ExecuteAsync(document, telegram).SingleAsync();
+
+                // Then
+                result["Foo"].ShouldBe(1);
+                result["Bar"].ShouldBe("baz");
+            }
+
+            /// <summary>
+            /// Sets the metadata from the requests with proxy.
+            /// </summary>
+            [TestCase("1234567:4TT8bAc8GHUspu3ERYn-KGcvsvGB9u_n4ddy", "https://example.org", 8080, "USERNAME", "PASSWORD")]
+            public async Task SetsMetadataWithProxy(string accessToken, string proxyHost, int proxyPort, string proxyUserName, string proxyPassword)
+            {
+                // Given
+                TestDocument document = new TestDocument();
+                WebProxy proxy = new WebProxy(proxyHost, proxyPort)
+                {
+                    // Credentials if needed:
+                    Credentials = new NetworkCredential(proxyUserName, proxyPassword)
+                };
+                IModule telegram = new ReadTelegram(accessToken, proxy)
+                    .WithRequest("Foo", (ctx, yt) => 1)
+                    .WithRequest("Bar", (doc, ctx, yt) => "baz");
+
+                // When
+                TestDocument result = await ExecuteAsync(document, telegram).SingleAsync();
+
+                // Then
+                result["Foo"].ShouldBe(1);
+                result["Bar"].ShouldBe("baz");
+            }
+        }
+    }
+}
