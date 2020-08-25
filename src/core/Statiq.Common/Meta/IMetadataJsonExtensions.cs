4b2986f221f27892e963ddc5e1b8168d37e8c3b0
Dave Glick <dave@daveaglick.com>
2020-08-25 09:20:14 -0400

    Added IMetadata.ToJson() extensions

---
+++ src/core/Statiq.Common/Meta/IMetadataJsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.Json;
+
+namespace Statiq.Common
+{
+    public static class IMetadataJsonExtensions
+    {
+        public static string ToJson(this IMetadata metadata, JsonSerializerOptions options = null) =>
+            JsonSerializer.Serialize<IReadOnlyDictionary<string, object>>(metadata, options);
+
+        public static string ToJson(this IEnumerable<IMetadata> metadata, JsonSerializerOptions options = null) =>
+            JsonSerializer.Serialize(metadata?.Cast<IReadOnlyDictionary<string, object>>().ToArray(), options);
+    }
+}
---
+++ tests/core/Statiq.Common.Tests/Meta/IMetadataJsonExtensionsFixture.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+using Statiq.Core;
+using Statiq.Testing;
+
+namespace Statiq.Common.Tests.Meta
+{
+    [TestFixture]
+    public class IMetadataJsonExtensionsFixture : BaseFixture
+    {
+        public class ToJsonTests : IMetadataJsonExtensionsFixture
+        {
+            [Test]
+            public void SerializesMetadata()
+            {
+                // Given
+                TestMetadata metadata = new TestMetadata
+                {
+                    { "A", "a" },
+                    { "B", "b" }
+                };
+
+                // When
+                string json = metadata.ToJson();
+
+                // Then
+                json.ShouldBe(@"{""A"":""a"",""B"":""b""}");
+            }
+
+            [Test]
+            public void SerializesNullMetadata()
+            {
+                // Given
+                TestMetadata metadata = null;
+
+                // When
+                string json = metadata.ToJson();
+
+                // Then
+                json.ShouldBe("null");
+            }
+
+            [Test]
+            public void SerializesMetadataEnumerable()
+            {
+                // Given
+                TestMetadata[] metadata = new TestMetadata[]
+                {
+                    new TestMetadata
+                    {
+                        { "A", "a" },
+                        { "B", "b" }
+                    },
+                    new TestMetadata
+                    {
+                        { "X", "x" },
+                        { "Y", "y" }
+                    }
+                };
+
+                // When
+                string json = metadata.ToJson();
+
+                // Then
+                json.ShouldBe(@"[{""A"":""a"",""B"":""b""},{""X"":""x"",""Y"":""y""}]");
+            }
+
+            [Test]
+            public void SerializesNullMetadataEnumerable()
+            {
+                // Given
+                TestMetadata[] metadata = null;
+
+                // When
+                string json = metadata.ToJson();
+
+                // Then
+                json.ShouldBe("null");
+            }
+
+            [Test]
+            public void SerializesMetadataEnumerableWithNull()
+            {
+                // Given
+                TestMetadata[] metadata = new TestMetadata[]
+                {
+                    new TestMetadata
+                    {
+                        { "A", "a" },
+                        { "B", "b" }
+                    },
+                    null
+                };
+
+                // When
+                string json = metadata.ToJson();
+
+                // Then
+                json.ShouldBe(@"[{""A"":""a"",""B"":""b""},null]");
+            }
+        }
+    }
+}
