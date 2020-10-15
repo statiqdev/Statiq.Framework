4679983adcd9f937f366a23c449a0e03d9614595
Dave Glick <dave@daveaglick.com>
2020-10-15 09:04:34 -0400

    Removed the older IAnalyerContext.Add() extension (should only add directly)

--- src/core/Statiq.Common/Analyzers/IAnalyzerContextExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace Statiq.Common
-{
-    public static class IAnalyzerContextExtensions
-    {
-        public static void Add(this IAnalyzerContext context, string message) => context.AddAnalyzerResult(null, message);
-    }
-}
