d38acdeb7e9de50f2407440ee006069d13275e3e
Dave Glick <dave@daveaglick.com>
2020-10-19 21:25:38 -0400

    Adds test objects for analyzers

---
+++ src/core/Statiq.Testing/Analyzers/TestAnalyzerContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Statiq.Common;
+
+namespace Statiq.Testing
+{
+    public class TestAnalyzerContext : TestExecutionContext, IAnalyzerContext
+    {
+        public TestAnalyzerContext()
+        {
+        }
+
+        public TestAnalyzerContext(params IDocument[] inputs)
+            : base(inputs)
+        {
+        }
+
+        public TestAnalyzerContext(IEnumerable<IDocument> inputs)
+            : base(inputs)
+        {
+        }
+
+        public LogLevel LogLevel { get; set; } = LogLevel.Warning;
+
+        public ConcurrentBag<TestAnalyzerResult> AnalyzerResults { get; } = new ConcurrentBag<TestAnalyzerResult>();
+
+        public void AddAnalyzerResult(IDocument document, string message) => AnalyzerResults.Add(new TestAnalyzerResult(document, message));
+
+        public LogLevel GetLogLevel(IDocument document) => LogLevel;
+    }
+}
---
+++ src/core/Statiq.Testing/Analyzers/TestAnalyzerResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Statiq.Common;
+
+namespace Statiq.Testing
+{
+    public class TestAnalyzerResult
+    {
+        public TestAnalyzerResult(IDocument document, string message)
+        {
+            Document = document;
+            Message = message;
+        }
+
+        public IDocument Document { get; }
+
+        public string Message { get; }
+    }
+}
