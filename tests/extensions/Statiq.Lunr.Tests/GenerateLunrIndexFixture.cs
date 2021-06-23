88597458f1a8ac38a8eee2f97fa6c879de58811e
Dave Glick <dave@daveaglick.com>
2021-06-23 10:23:28 -0400

    Adds unit tests for Statiq.Lunr

---
+++ tests/extensions/Statiq.Lunr.Tests/GenerateLunrIndexFixture.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Statiq.Common;
+using Statiq.Testing;
+
+namespace Statiq.Lunr.Tests
+{
+    [TestFixture]
+    public class GenerateLunrIndexFixture : BaseFixture
+    {
+        public class ExecuteTests : GenerateLunrIndexFixture
+        {
+            [Test]
+            public async Task AddsDocumentsToIndex()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestDocument b = new TestDocument((NormalizedPath)"b.html", "Buzz")
+                {
+                    { Keys.Title, "Bar" }
+                };
+                GenerateLunrIndex module = new GenerateLunrIndex();
+
+                // When
+                TestDocument result = await ExecuteAsync(new[] { a, b }, module).SingleAsync();
+
+                // Then
+                result.Content.ShouldContain(@"url:'/a/a.html'");
+                result.Content.ShouldContain(@"url:'/b.html'");
+            }
+
+            [Test]
+            public async Task HidesDocumentFromSearchIndex()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestDocument b = new TestDocument((NormalizedPath)"b.html", "Buzz")
+                {
+                    { Keys.Title, "Bar" },
+                    { LunrKeys.HideFromSearchIndex, true }
+                };
+                GenerateLunrIndex module = new GenerateLunrIndex();
+
+                // When
+                TestDocument result = await ExecuteAsync(new[] { a, b }, module).SingleAsync();
+
+                // Then
+                result.Content.ShouldContain(@"url:'/a/a.html'");
+                result.Content.ShouldNotContain(@"url:'/b.html'");
+            }
+
+            [Test]
+            public async Task DefaultOutputDestination()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestDocument b = new TestDocument((NormalizedPath)"b.html", "Buzz")
+                {
+                    { Keys.Title, "Bar" }
+                };
+                GenerateLunrIndex module = new GenerateLunrIndex();
+
+                // When
+                TestDocument result = await ExecuteAsync(new[] { a, b }, module).SingleAsync();
+
+                // Then
+                result.Destination.ShouldBe("searchIndex.js");
+            }
+
+            [Test]
+            public async Task SetsOutputDestination()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestDocument b = new TestDocument((NormalizedPath)"b.html", "Buzz")
+                {
+                    { Keys.Title, "Bar" }
+                };
+                GenerateLunrIndex module = new GenerateLunrIndex().WithDestination("search/index.js");
+
+                // When
+                TestDocument result = await ExecuteAsync(new[] { a, b }, module).SingleAsync();
+
+                // Then
+                result.Destination.ShouldBe("search/index.js");
+            }
+
+            [Test]
+            public async Task DoesNotIncludeHostByDefault()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestExecutionContext context = new TestExecutionContext();
+                context.Settings.Add(Keys.Host, "bar.com");
+                GenerateLunrIndex module = new GenerateLunrIndex();
+
+                // When
+                TestDocument result = await ExecuteAsync(a, context, module).SingleAsync();
+
+                // Then
+                result.Content.ShouldContain(@"url:'/a/a.html'");
+            }
+
+            [Test]
+            public async Task IncludesHost()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestExecutionContext context = new TestExecutionContext();
+                context.Settings.Add(Keys.Host, "bar.com");
+                GenerateLunrIndex module = new GenerateLunrIndex().IncludeHost();
+
+                // When
+                TestDocument result = await ExecuteAsync(a, context, module).SingleAsync();
+
+                // Then
+                result.Content.ShouldContain(@"url:'http://bar.com/a/a.html'");
+            }
+
+            [Test]
+            public async Task TransformsScript()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestExecutionContext context = new TestExecutionContext();
+                context.Settings.Add(Keys.Host, "bar.com");
+                GenerateLunrIndex module = new GenerateLunrIndex().WithScript((b, ctx) =>
+                {
+                    b.Append("HI!");
+                    return b.ToString();
+                });
+
+                // When
+                TestDocument result = await ExecuteAsync(a, context, module).SingleAsync();
+
+                // Then
+                result.Content.ShouldEndWith("HI!");
+            }
+
+            [Test]
+            public async Task UsesCustomIndexItem()
+            {
+                // Given
+                TestDocument a = new TestDocument((NormalizedPath)"a/a.html", "Fizz")
+                {
+                    { Keys.Title, "Foo" }
+                };
+                TestDocument b = new TestDocument((NormalizedPath)"b.html", "Buzz")
+                {
+                    { Keys.Title, "Bar" },
+                    {
+                        LunrKeys.LunrIndexItem,
+                        new LunrIndexItem("/c/c.html", "Baz", "Fizz")
+                    }
+                };
+                GenerateLunrIndex module = new GenerateLunrIndex();
+
+                // When
+                TestDocument result = await ExecuteAsync(new[] { a, b }, module).SingleAsync();
+
+                // Then
+                result.Content.ShouldContain(@"url:'/a/a.html'");
+                result.Content.ShouldNotContain(@"url:'/b.html'");
+                result.Content.ShouldContain(@"url:'/c/c.html'");
+                result.Content.ShouldContain(@"title:""Baz""");
+                result.Content.ShouldContain(@"content:""Fizz""");
+            }
+        }
+    }
+}
---
+++ tests/extensions/Statiq.Lunr.Tests/LunrIndexDocItemFixture.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Statiq.Common;
+using Statiq.Testing;
+
+namespace Statiq.Lunr.Tests
+{
+    [TestFixture]
+    public class LunrIndexDocItemFixture : BaseFixture
+    {
+        public class ConstructorTests : LunrIndexDocItemFixture
+        {
+            [Test]
+            public void ThrowsForNullDocument()
+            {
+                // Given, When, Then
+                Should.Throw<ArgumentNullException>(() => new LunrIndexDocItem(null));
+            }
+
+            [Test]
+            public void ThrowsForNullDocumentAlternate()
+            {
+                // Given, When, Then
+                Should.Throw<ArgumentNullException>(() => new LunrIndexDocItem(null, "Foo", "Bar"));
+            }
+        }
+
+        public class TitleTests : LunrIndexDocItemFixture
+        {
+            [Test]
+            public void GetsTitleFromDocument()
+            {
+                // Given
+                TestDocument document = new TestDocument
+                {
+                    { Keys.Title, "Foo" }
+                };
+                LunrIndexDocItem item = new LunrIndexDocItem(document);
+
+                // When
+                string title = item.Title;
+
+                // Then
+                title.ShouldBe("Foo");
+            }
+
+            [Test]
+            public void ConstructorOverridesDocumentTitle()
+            {
+                // Given
+                TestDocument document = new TestDocument
+                {
+                    { Keys.Title, "Foo" }
+                };
+                LunrIndexDocItem item = new LunrIndexDocItem(document, "Bar", "Bazz");
+
+                // When
+                string title = item.Title;
+
+                // Then
+                title.ShouldBe("Bar");
+            }
+        }
+
+        public class ContentTests : LunrIndexDocItemFixture
+        {
+            [Test]
+            public async Task GetsContentFromDocument()
+            {
+                // Given
+                TestDocument document = new TestDocument("Fizz");
+                LunrIndexDocItem item = new LunrIndexDocItem(document);
+
+                // When
+                string content = await item.GetContentAsync();
+
+                // Then
+                content.ShouldBe("Fizz");
+            }
+
+            [Test]
+            public async Task ConstructorOverridesDocumentContent()
+            {
+                // Given
+                TestDocument document = new TestDocument("Fizz");
+                LunrIndexDocItem item = new LunrIndexDocItem(document, "Bar", "Bazz");
+
+                // When
+                string content = await item.GetContentAsync();
+
+                // Then
+                content.ShouldBe("Bazz");
+            }
+        }
+    }
+}
