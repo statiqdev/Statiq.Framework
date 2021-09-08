682dc8c7f2dc460b122e16a78073b0ed6e23092f
Mattias Karlsson <matkar@wcom.se>
2021-09-08 10:22:42 +0200

    Switch from System to Microsoft SqlClient

--- src/core/Statiq.Core/Modules/Metadata/ReadSql.cs
+++ src/core/Statiq.Core/Modules/Metadata/ReadSql.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Statiq.Common;
 
 namespace Statiq.Core
