2b36ff6a947d3806c10ac013cfbf3cd457040b70
Dave Glick <dave@daveaglick.com>
2022-02-10 17:31:14 -0500

    Adding the missing NullTextWriter

---
+++ src/core/Statiq.Common/Util/NullTextWriter.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Statiq.Common
+{
+    public class NullTextWriter : TextWriter
+    {
+        public static NullTextWriter Instance { get; } = new NullTextWriter();
+
+        private NullTextWriter()
+        {
+        }
+
+        public override Encoding Encoding => Encoding.Default;
+    }
+}
