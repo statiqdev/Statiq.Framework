7f783b747214ecb06f1ddc47bafcb59926539434
Mattias Karlsson <matkar@wcom.se>
2023-11-18 15:09:14 +0100

    Fix tests that can break on non en-US i.e. lunar indexes

--- src/core/Statiq.Common/Meta/TypeConversion/MetadataTypeConverter{T}.cs
+++ src/core/Statiq.Common/Meta/TypeConversion/MetadataTypeConverter{T}.cs
@@ -23,6 +23,10 @@ namespace Statiq.Common
         public static bool TryConvert(object value, out T result) =>
             UniversalTypeConverter.TryConvertTo(value, out result);
 
+        // This is where the magic happens via https://github.com/t-bruning/UniversalTypeConverter
+        public static bool TryConvertInvariant(object value, out T result) =>
+            UniversalTypeConverter.TryConvertTo(value, out result, System.Globalization.CultureInfo.InvariantCulture);
+
         private static IEnumerable<T> ConvertEnumerable(IEnumerable enumerable)
         {
             foreach (object value in enumerable)
